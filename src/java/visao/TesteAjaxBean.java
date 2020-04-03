d5eec1f72b9510300d56f445ed1f07441d2c6548
Plebani <Plebani@DESKTOP-EOLKVDB>
2020-04-03 15:01:45 -0300

    Incluso ajax nas telas de Lancamento

---
+++ src/java/visao/TesteAjaxBean.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package visao;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author Plebani
+ */
+@ManagedBean
+public class TesteAjaxBean {
+    private String descricao;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+        
+}
